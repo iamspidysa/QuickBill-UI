cb737d89b0f3e1f619cbd890772b198237466af3
iamspidysa <iamspidysa@users.noreply.github.com>
2026-03-13 23:06:11 +0530

    Added Order History Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ManageItems from "./pages/ManageItems/ManageItems";
 import Explore from "./pages/Explore/Explore";
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login/Login";
+import OrderHistory from "./pages/OrderHistory/OrderHistory";
 
 export default function App() {
   const location = useLocation();
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/items" element={<ManageItems/>} />
         <Route path="/explore" element={<Explore/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/orders" element={<OrderHistory/>} />
         <Route path="/" element={<Dashboard/>} />
       </Routes>
     </div>
--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -62,6 +62,11 @@ const Menubar = () => {
               Manage Users
             </Link>
           </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/orders">
+              Order History
+            </Link>
+          </li>
         </ul>
         {/* We Will add Dropdown for userProfile. */}
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
